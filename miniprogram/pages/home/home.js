ff1f04cf7e056c00b552bf26f6410104ee37ef22
blackmood <yuexingphp@gmail.com>
2019-01-28 16:07:12 +0800

    新增弹幕

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,18 +1,20 @@
 // miniprogram/pages/home/home.js
+var page = undefined;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    time:null
+    time:null,
+    doommData: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    page = this;
   },
 
   /**
@@ -20,6 +22,12 @@ Page({
    */
   onReady: function () {
 
+  },
+  bindbt: function () {
+    doommList.push(getList());
+    this.setData({
+      doommData: doommList
+    })
   },
 
   /**
@@ -42,7 +50,7 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
@@ -73,3 +81,42 @@ Page({
 
   }
 })
+
+//弹幕文字
+var arr = ['老婆，我爱你','亲爱的，祝你开心呀！','宝贝儿！','嘻嘻嘻~','我错了~~','就快一年啦，宝宝！']
+
+//弹幕列表
+function getList(){
+  //随机数
+  let num = Math.floor(Math.random() * arr.length)
+  return new Doomm(arr[num], Math.ceil(Math.random() * 100), Math.ceil(Math.random() * 10), getRandomColor())
+}
+
+var doommList = [];
+var i = 0;
+class Doomm {
+  constructor(text, top, time, color) {
+    this.text = text;
+    this.top = top;
+    this.time = time;
+    this.color = color;
+    this.display = true;
+    let that = this;
+    this.id = i++;
+    setTimeout(function () {
+      doommList.splice(doommList.indexOf(that), 1);
+      page.setData({
+        doommData: doommList
+      })
+    }, this.time * 1000)
+  }
+}
+function getRandomColor() {
+  let rgb = []
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length == 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
